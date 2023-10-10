beadb748b528741e66f472e019a014ea15891d35
Дмитрий Павлов <d.pavlov@dunice.net>
2023-10-10 11:09:52 +0300

    add task

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,76 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <CustomForm
+      id="form"
+      @submit="onFormSubmit"
+      :class="['form', layout || 'vertical']"
+  >
+    {{ getTransformedValue(variable) }}
+
+    <template #values>
+      <ul>
+        <li
+            v-for="value in formValues"
+            :key="generateRandomString()"
+        >
+          {{ value }}
+        </li>
+      </ul>
+    </template>
+
+    <CustomInput v-model="formValues.first" ref="input" />
+    <CustomInput v-model.lazy="formValues.third" />
+  </CustomForm>
 </template>
 
+<script>
+import generateRandomString from 'generator'; // always generates unique string
+import expensiveComputations from 'computation';
+const initialFormValues = {
+  first: null,
+  second: null,
+};
+export default {
+  props: {
+    layout: String // Required. Possible values: vertical | horizontal
+  },
+
+  data () {
+    return { variable: 'example' }
+  },
+  computed: {
+    formValues () {
+      return this.$store.state.formValues;
+    }
+  },
+  watch: {
+    formValues (value, prevValue) {
+      if (value.first !== prevValue.first) { console.log('changed') }
+    },
+
+    '$refs.input.innerValue' (value) { console.log('changed') }
+  },
+  created () {
+    document.querySelector('#form').classList.add(this.layout);
+    this.$store.state.formValues = initialFormValues;
+  },
+  mounted () {
+    this.$root.$on('form:data:set', () => { console.log('changed'); });
+  },
+  methods: {
+    onFormSubmit (event) { event.preventDefault(); },
+    getTransformedValue (value) { return expensiveComputations(value); }
+  }
+};
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+.vertical >>> .input {...}
 </style>
+
+<style>
+.form {...}
+</style>
+
+<docs>
+Component docs: ...
+</docs>
